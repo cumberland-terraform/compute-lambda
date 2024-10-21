da2ca9338a6cb43b5a9e711e1356abeff58377db
grant <grant.moore@maryland.gov>
2024-10-21 08:33:35 -0400

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "secret" {
+output "lambda" {
     value                   = {
         arn                 = aws_lambda_function.this.arn
         invoke_arn          = aws_lambda_function.this.invoke_arn 
