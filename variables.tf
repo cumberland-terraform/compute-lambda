443b043242d9886cadff553ace35a96d9897556d
Grant Moore <gmoore@Grants-MacBook-Pro.local>
2025-07-27 19:34:07 -0400

    update platform variable

--- variables.tf
+++ variables.tf
@@ -1,14 +1,8 @@
 variable "platform" {
-  description             = "Platform metadata configuration object. See [Platform Module] (https://source.mdthink.maryland.gov/projects/etm/repos/mdt-eter-platform/browse) for detailed information about the permitted values for each field."
+  description             = "Platform metadata configuration object."
   type                    = object({
-    aws_region            = string 
-    account               = string
-    acct_env              = string
-    agency                = string
-    app                   = string
-    program               = string
-    availability_zones    = list(string)
-    pca                   = string
+    client                = string 
+    environment           = string
   })
 }
 
